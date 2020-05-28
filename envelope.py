8951386568b6e5be536ea2526b0fd0cb4d859252
MajorThird <test@testtesttest.org>
2020-05-28 12:18:10 +0200

    clean up

--- envelope.py
+++ envelope.py
@@ -1,4 +1,4 @@
-from math_functions import LinearConnector
+from math_functions import LinearInterpolator
 
 # A class for an envelope generator.
 
@@ -9,7 +9,7 @@ class Envelope(object):
             attack_time=0.01,
             decay_time=0.11,
             release_time=1.1,
-            dt=1.0 / 44100.0,
+            dt=1.0/44100.0,
             after_decay_level=0.2,
             start_level=0.0,
             attack_level=1.0,
@@ -41,12 +41,12 @@ class Envelope(object):
         # callable to connect start_level and attack_level
         point_a = (0.0, self.start_level)
         point_b = (self.attack_time, self.attack_level)
-        self.attack_function = LinearConnector(point_a, point_b)
+        self.attack_function = LinearInterpolator(point_a, point_b)
 
         # callable to connect attack_level and after_decay_level
         point_a = (self.attack_time, self.attack_level)
         point_b = (self.attack_time + self.decay_time, self.after_decay_level)
-        self.decay_function = LinearConnector(point_a, point_b)
+        self.decay_function = LinearInterpolator(point_a, point_b)
 
 
         # callable to reach after_release_level; is defined later
@@ -65,7 +65,7 @@ class Envelope(object):
         self.start_level = start_level
         point_a = (0.0, self.start_level)
         point_b = (self.attack_time, self.attack_level)
-        self.attack_function = LinearConnector(point_a, point_b)
+        self.attack_function = LinearInterpolator(point_a, point_b)
 
 
     def reset_and_start(self):
@@ -100,12 +100,9 @@ class Envelope(object):
         point_a = (self._release_hit_time, self._release_hit_level)
         point_b = (self._release_hit_time + self.release_time, self.after_release_level)
 
-        self.release_function = LinearConnector(point_a, point_b)
+        self.release_function = LinearInterpolator(point_a, point_b)
 
 
-
-    # get the current value
-    #def get(self):
     def __call__(self):
         if not self.is_active:
             return 0.0
@@ -119,44 +116,7 @@ class Envelope(object):
         else:
             if self.internal_time < self._release_hit_time + self.release_time:
                 value = self.release_function(self.internal_time)
-                # if self.release_time == 11.81:
-                #     if self.internal_time - self._release_hit_time < 0.5 * self.release_time:
-                #         print(self.release_time, self._release_hit_time + self.release_time, self.internal_time, value)
             else:
-                #print(self.internal_time, "hallo")
                 value = self.after_release_level
         self.current_value = value
         return value
-
-
-# Do some testing.
-def main():
-    dt = 1.0 / 44000.0
-    e = Envelope(
-        dt=dt,
-        attack_time=0.15,
-        decay_time=0.20,
-        release_time=1.0,
-        after_decay_level=0.1)
-    release_set = False
-    e.reset_and_start()
-    def waveform(x): return 1.0 if x < 0.5 else -1.0
-
-    import oscillator
-    o = oscillator.Oscillator(frequency=940.0, dt=dt, waveform=waveform)
-
-    with open("test.dat", "w") as outfile:
-        for i in range(165700):
-            value = e() * o()
-            time = e.internal_time
-            outfile.write("%f %f\n" % (time, value))
-            # print(time, e.is_finished())
-            if time > 0.5 and not release_set:
-                e.set_release()
-                release_set = True
-            e.step()
-            o.step()
-
-
-if __name__ == "__main__":
-    main()
