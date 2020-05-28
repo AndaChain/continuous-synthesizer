9ee099509479ec7a8e88da9f28323d9fb40e38f7
MajorThird <test@testtesttest.org>
2020-05-28 12:17:43 +0200

    rename stuff

--- math_functions.py
+++ math_functions.py
@@ -1,26 +1,13 @@
 from scipy.interpolate import interp1d
 import math
 
-# get the value at position t between a[0] and b[0]
-# using linear interpolation.
-# a and b are array-like.
-# t is float.
-# def connect_linear(a, b, t):
-#     x = [a[0], b[0]]
-#     y = [a[1], b[1]]
-#     f = interp1d(x, y, kind='linear')
-#     value = f(t)
-#     return value
-
-
 def connect_linear(a, b, t):
-    steigung = (b[1] - a[1]) / (b[0] - a[0])
-    value = a[1] + (t-a[0]) * steigung
-    # print(steigung, value, t)
-    return value
+    slope = (b[1] - a[1]) / (b[0] - a[0])
+    res = a[1] + (t - a[0]) * slope
+    return res
 
 
-class LinearConnector(object):
+class LinearInterpolator(object):
     def __init__(self, point_a=None, point_b=None):
         self.point_a=point_a
         self.point_b=point_b
