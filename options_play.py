f5d45418d059c9c2296961bf26bf7010e09b3799
MajorThird <test@testtesttest.org>
2020-05-28 12:11:14 +0200

    implement loading from textfile

--- options_play.py
+++ options_play.py
@@ -1,120 +1,76 @@
 import os
-# import argparse
-# import configparser
-# import misc_helpers
-#
-# def get_config(filename):
-#     """
-#     All settings are stored in an external text file.
-#     """
-#     config = configparser.ConfigParser()
-#     config.read(filename)
-#     return config
-#
-#
-# def get_parser():
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument("-c", "--config",
-#         required=False,
-#         default="options.cfg",
-#         help="path to program options file")
-#     parser.add_argument("-s", "--song",
-#         required=False,
-#         help="use this song instead of the one in the config file [don't write the extension .ly]")
-#     parser.add_argument("-r", "--right",
-#         required=False,
-#         help="create song for the right hand [possible values: 0,1]")
-#     parser.add_argument("-l", "--left",
-#         required=False,
-#         help="create song for the left hand [possible values: 0,1]")
-#     parser.add_argument("-i", "--infolder",
-#         required=False,
-#         help="folder containing the lilypond files")
-#     return parser
-#
-#
-#
-#
-# def get_options_from_config(config):
-#     true_strings = misc_helpers.get_true_strings()
-#
-#     options = {}
-#     options["song"] = config["MAKE_OPTIONS"]["song"]
-#     options["play_right"] = True if config["MAKE_OPTIONS"]["play_right"] in true_strings else False
-#     options["play_left"] = True if config["MAKE_OPTIONS"]["play_left"] in true_strings else False
-#     options["in_folder"] = misc_helpers.normalize_folder_string(config["MAKE_OPTIONS"]["in_folder"])
-#     options["out_folder"] = misc_helpers.normalize_folder_string(config["MAKE_OPTIONS"]["out_folder"])
-#     options["image_width"] = int(config["SHARED_OPTIONS"]["screen_width"])
-#     options["image_height"] = int(config["SHARED_OPTIONS"]["screen_height"])
-#     options["image_resolution"] = int(config["MAKE_OPTIONS"]["image_resolution"])
-#     return options
-#
-#
-# def replace_options_from_command_line(options, args):
-#     true_strings = misc_helpers.get_true_strings()
-#     if args["song"]:
-#         options["song"] = args["song"]
-#     if args["left"]:
-#         options["play_left"] = True if args["left"] in true_strings else False
-#
-#
-# def create_complete_paths(options):
-#     hand_string = misc_helpers.get_folder_hand_string(options)
-#     options["output_folder_complete"] = os.path.join(options["out_folder"], options["song"], hand_string) + "/"
-#     options["lily_file"] = os.path.join(options["in_folder"], options["song"] + ".ly")
-#
-#
-# def get_options():
-#     parser = get_parser()
-#     parsed_arguments = vars(parser.parse_args())
-#     filename = parsed_arguments["config"]
-#     config = get_config(filename)
-#     options = get_options_from_config(config)
-#
-#
-#     replace_options_from_command_line(options, parsed_arguments)
-#     create_complete_paths(options)
-#     options["staff_activations"] = [options["play_right"], options["play_left"]]
-#
-#     return options
+import argparse
+import configparser
 
-def normalize_folder_string(folder):
+
+def get_config(filename):
     """
-    Make sure that the folder names given in the input file
-    contain / at the end.
+    All settings are stored in an external text file.
     """
-    folder = folder.strip()
-    if folder[-1] == "/":
-        return folder
-    else:
-        return folder + "/"
+    config = configparser.ConfigParser()
+    config.read(filename)
+    return config
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config",
+        required=False,
+        default="options.cfg",
+        help="path to program options file")
+    parser.add_argument("-x", "--sizex", required=False)
+    parser.add_argument("-y", "--sizey", required=False)
+    parser.add_argument("-f", "--wildcard", required=False)
+    parser.add_argument("-w", "--waveform", required=False)
+    return parser
+
+
+
+def get_options_from_config(config):
+    options = {}
+    config = config["DEFAULT"]
+    options["waveform"] = config["waveform"]
+    options["size_x"] = int(config["size_x"])
+    options["size_y"] = int(config["size_y"])
+    options["frequency_board"] = config["frequency_board"]
+    options["bass_frequencies_filename"] = config["bass_frequencies_filename"]
+    options["transition_size"] = float(config["transition_size"])
+    options["transposition_factor"] = float(config["transposition_factor"])
+    options["no_of_voices"] = int(config["no_of_voices"])
+    options["attack_time"] = float(config["attack_time"])
+    options["release_time"] = float(config["release_time"])
+    options["decay_time"] = float(config["decay_time"])
+    options["after_decay_level"] = float(config["after_decay_level"])
+    options["bass_attack_time"] = float(config["bass_attack_time"])
+    options["bass_release_time"] = float(config["bass_release_time"])
+    options["bass_decay_time"] = float(config["bass_decay_time"])
+    options["bass_after_decay_level"] = float(config["bass_after_decay_level"])
+    options["wildcard_frequency"] = float(config["wildcard_frequency"])
+    options["samplerate"] = int(config["samplerate"])
+    options["volume"] = float(config["volume"])
+    return options
+
+
+def replace_options_from_command_line(options, args):
+    if args["sizex"]:
+        options["size_x"] = int(args["sizex"])
+    if args["sizey"]:
+        options["size_y"] = int(args["sizey"])
+    if args["wildcard"]:
+        options["wildcard_frequency"] = float(args["wildcard"])
+    if args["waveform"]:
+        options["waveform"] = args["waveform"]
+
+
 
 
 
 def get_options():
-    options = {}
-    #options["device"] = 0
-    options["waveform"] = "square"
-    options["waveform"] = "sine"
-    options["waveform"] = "triangle"
-    options["waveform"] = "sawtooth"
-    options["size_x"] = 1333
-    options["size_y"] = 733
-    options["frequency_board"] = os.path.join("./frequency_boards/", "test.txt")
-    options["transition_size"] = 0.8  # large value means big size
-    options["transposition_factor"] = 1.0
-    options["no_of_voices"] = 2
-    options["attack_time"] = 0.95
-    options["release_time"] = 0.94
-    options["decay_time"] = 0.1
-    options["after_decay_level"] = 1.0
+    parser = get_parser()
+    parsed_arguments = vars(parser.parse_args())
+    filename = parsed_arguments["config"]
+    config = get_config(filename)
+    options = get_options_from_config(config)
+    replace_options_from_command_line(options, parsed_arguments)
     options["no_of_bass_voices"] = 1
-    options["bass_attack_time"] = 0.8
-    options["bass_release_time"] = 0.3
-    options["bass_decay_time"] = 0.01
-    options["bass_after_decay_level"] = 1.0
-    options["wildcard_frequency"] = 90.0
-    options["bass_frequencies_filename"] = os.path.join("./bass_frequencies/", "test.txt")
-
-    # options[""] =
     return options
